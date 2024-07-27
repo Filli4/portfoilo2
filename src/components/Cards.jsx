d79bd06f35cdc3aa60cb2e4417102b26fd0b56c9
Your Name <you@example.com>
2024-07-27 03:24:51 +0200

    pop out info done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,132 +1,34 @@
-/* "use client"
-import { useState } from "react";
-import Link from"next/link";
-import Footer from "./Footer";
-import "./Card.css";
-
-function Cards(params) {
-
-  const [showDetails, setShowDetails] = useState(false);
-
-  const toggleDetails = () => {
-      setShowDetails(!showDetails);
-  };
-
-
-    return(<>
-
-      
-
-
-
-       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4  mt-8 mb-4 
-       mx-2 xl:grid-cols-4 ">
-
-    <div  className="bg-white dark:bg-gray-700/[30%] dark:border-x-4 dark:rounded-3xl dark:border-lime-200/35  rounded-lg shadow-md p-4 ">
-      <img src="https://via.placeholder.com/400" alt="Card Image" className="w-full mb-4  rounded-t-lg " />
-      <h2 className="text-xl font-semibold mb-4">To Do List</h2>
-      <p className="text-gray-600 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit&rsquo; Eligendi suscipit quia illo quo at ad&rsquo;</p>
-      
-      <div className=" grid grid-cols-2 justify-items-center items-end">
-      
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-full mt-4 hover:bg-blue-700 transition duration-300" onClick={toggleDetails}>More Info</button>
-      {showDetails && (
-         <div className="popout-container flex justify-center items-center ">
-         <div className="details flex flex-col justify-center items-center ml-16">
-           <div className="bg-white rounded-t-md p-4">
-             <h2 className="text-xl font-semibold mb-4">To Do List</h2>
-             <p className="text-gray-600 mb-4 ">Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad</p>
-             <button className="font-semibold text-l bg-slate-400 px-4 py-2 rounded" onClick={toggleDetails}>Close</button>
-           </div>
-         </div>
-       </div>
-       
-       )}
-      
-      <Link href="/Projects"> 
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-full mt-4 hover:bg-blue-700 transition duration-300">Web Site</button>
-      </Link></div>
-    </div>
-    
-    <div  className="bg-white rounded-lg shadow-md p-4 dark:bg-gray-700/[30%] dark:border-x-4 dark:rounded-3xl dark:border-lime-200/35 ">
-      <img src="https://via.placeholder.com/400" alt="Card Image" className="w-full mb-4  rounded-t-lg " />
-      <h2 className="text-xl font-semibold mb-4">Todo-List</h2>
-      <p id="" className="text-gray-600 text-wrap min-h-28 dark:text-gray-300">In this project i did a todo list&rsquo; it&apos;s a simple project for a quick list you want to do&rsquo; 
-                                   You can Add a list&lsquo; you can hit the done button or even remove the list that you want&rsquo;   </p>
-    
-      <div className=" grid grid-cols-2 justify-items-center items-end">
-      <Link href="/Projects"> 
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-full mt-4 hover:bg-blue-700 transition duration-300">More Info</button>
-      </Link>
-      <Link href=" https:> 
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-full mt-4 hover:bg-blue-700 transition duration-300">Web Site</button>
-      </Link></div>
-    </div>    
-    
-    <div  className="bg-white rounded-lg shadow-md p-4 dark:bg-gray-700/[30%] dark:border-x-4 dark:rounded-3xl dark:border-lime-200/35 ">
-      <img src="https://via.placeholder.com/400" alt="Card Image" className="w-full mb-4  rounded-t-lg " />
-      <h2 className="text-xl font-semibold mb-4">Black-Jack</h2>
-      <p className="text-gray-600 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad</p>
-    
-      <div className=" grid grid-cols-2 justify-items-center items-end">
-      <Link href="/Projects"> 
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-full mt-4 hover:bg-blue-700 transition duration-300">More Info</button>
-      </Link>
-      <Link href="https://black-jack-rbe1bldvy-filmons-projects-b17674df.vercel.app/" target="_blank"> 
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-full mt-4 hover:bg-blue-700 transition duration-300">Web Site</button>
-      </Link></div>
-    </div>  
-
-    
-    <div  className="bg-white rounded-lg shadow-md p-4 dark:bg-gray-700/[30%] dark:border-x-4 dark:rounded-3xl dark:border-lime-200/35 ">
-      <img src="https://via.placeholder.com/400" alt="Card Image" className="w-full mb-4  rounded-t-lg " />
-      <h2 className="text-xl font-semibold mb-4">Project Name</h2>
-      <p className="text-gray-600 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad</p>
-      
-      <div className=" grid grid-cols-2 justify-items-center items-end">
-      <Link href="/Projects"> 
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-full mt-4 hover:bg-blue-700 transition duration-300" >More Info</button>
-      </Link>
-      <Link href="/Projects"> 
-      <button className="bg-blue-600 text-white px-4 py-2 rounded-full mt-4 hover:bg-blue-700 transition duration-300">Web Site</button>
-      </Link></div>
-    </div>
-    
-      
-</div>
-</>
-    )
-}
-export default Cards;
- */
-"use client";
+"use client"
 import { useState } from "react";
 import Link from "next/link";
-import Footer from "./Footer";
 import "./Card.css";
 
 const cardData = [
   {
     title: "To Do List",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad",
+    more:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quas minima delectus nostrum corporis temporibus, possimus iste quae pariatur a . . .",
     image: "https://via.placeholder.com/400",
     link: "https://todolist-redux-delta.vercel.app/"
   },
   {
     title: "Black Jack",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad ...",
+    more:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quas minima delectus nostrum corporis temporibus, possimus iste quae pariatur a.",
     image: "https://via.placeholder.com/400",
     link: "https://black-jack-rbe1bldvy-filmons-projects-b17674df.vercel.app/"
   },
   {
     title: "Project 3",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad . . .",
+    more:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quas minima delectus nostrum corporis temporibus, possimus iste quae pariatur a.",
     image: "https://via.placeholder.com/400",
     link: "https://todolist-redux-delta.vercel.app/"
   },
   {
     title: "Project 4",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Eligendi suscipit quia illo quo at ad . . .",
+    more:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quas minima delectus nostrum corporis temporibus, possimus iste quae pariatur a.",
     image: "https://via.placeholder.com/400",
     link: "https://todolist-redux-delta.vercel.app/"
   }
@@ -144,9 +46,7 @@ function Cards() {
   };
 
   return (
-    <>
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 mb-4 mx-2 xl:grid-cols-4">
-      
       {cardData.map((card, index) => (
         <div key={index} className="bg-white dark:bg-gray-700/[30%] dark:border-x-4 dark:rounded-3xl dark:border-lime-200/35 rounded-lg shadow-md p-4">
           <img src={card.image} alt="Card Image" className="w-full mb-4 rounded-t-lg" />
@@ -155,13 +55,12 @@ function Cards() {
           <div className="grid grid-cols-2 justify-items-center items-end">
             <button className="bg-blue-600 text-white px-4 py-2 rounded-full mt-4 hover:bg-blue-700 transition duration-300" onClick={() => toggleDetails(index)}>More Info</button>
             {showDetails[index] && (
-              <div className="popout-container flex justify-center items-center">
-                <div className="details flex flex-col justify-center items-center ml-16">
-                  <div className="bg-white rounded-t-md p-4">
-                    <h2 className="text-xl font-semibold mb-4">{card.title}</h2>
-                    <p className="text-gray-600 mb-4">{card.description}</p>
-                    <button className="font-semibold text-l bg-slate-400 px-4 py-2 rounded" onClick={() => toggleDetails(index)}>Close</button>
-                  </div>
+              <div className={`popout-container  ${showDetails[index] ? 'active' : ''}`} onClick={() => toggleDetails(index)}>
+                <div className="details dark:bg-black dark:text-white dark:border-y-[3px] dark:rounded-3xl" onClick={(e) => e.stopPropagation()}>
+                  <img src={card.image} alt="Card Image" className="w-full mb-4 rounded-t-lg" />
+                  <h2 className="text-xl font-semibold  mb-4">{card.title}</h2>
+                  <p className="text-gray-600 dark:text-gray-300 mb-4">{card.more} </p>
+                  <button className="font-semibold " onClick={() => toggleDetails(index)}>Close</button>
                 </div>
               </div>
             )}
@@ -171,10 +70,7 @@ function Cards() {
           </div>
         </div>
       ))}
-     
     </div>
- 
-     </>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -30,24 +30,25 @@ function Navbar() {
         )}
       </button>
       <nav className={`flex-col sm:flex sm:flex-row sm:items-center gap-4 ${isMenuOpen ? 'flex' : 'hidden'} sm:flex ` }>
-        <Link href="/" className="text-gray-800 dark:text-gray-50 dark:hover:bg-blue-400 dark:hover:p-2 dark:hover:rounded-lg  hover:text-blue-600 ">Home</Link>
-        <Link href="/Projects" className="text-gray-800 dark:text-gray-50  dark:hover:bg-blue-400 dark:hover:p-2 dark:hover:rounded-lg hover:text-blue-600  ">Projects</Link>
-        <Link href="/About" className="text-gray-800 dark:text-gray-50 dark:hover:bg-blue-400 dark:hover:p-2 dark:hover:rounded-lg hover:text-blue-600  ">About Me</Link>
-        <Link href="/Skills" className="text-gray-800 dark:text-white dark:hover:bg-blue-400 dark:hover:p-2 dark:hover:rounded-lg hover:text-blue-600 ">Skills</Link>
+      <Link href="/" className="text-gray-800 dark:text-gray-50 dark:hover:bg-blue-800 dark:hover:rounded-lg dark:p-2 hover:text-black hover:bg-blue-400 p-2 rounded-lg">Home</Link>
+<Link href="/Projects" className="text-gray-800 dark:text-gray-50 dark:hover:bg-blue-800 dark:hover:rounded-lg dark:p-2 hover:text-black hover:bg-blue-400 p-2 rounded-lg">Projects</Link>
+<Link href="/About" className="text-gray-800 dark:text-gray-50 dark:hover:bg-blue-800 dark:hover:rounded-lg dark:p-2 hover:text-black hover:bg-blue-400 p-2 rounded-lg">About Me</Link>
+<Link href="/Skills" className="text-gray-800 dark:text-gray-50 dark:hover:bg-blue-800 dark:hover:rounded-lg dark:p-2 hover:text-black hover:bg-blue-400 p-2 rounded-lg">Skills</Link>
+
         <button 
         className=" rounded-sm duration-300 items-center "
         onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       >
-        {theme === 'dark' ? (
-          <svg className='justify-center items-center ' xmlns="http://www.w3.org/2000/svg" viewBox="-15 -4 110 100" width="34" height="35" fill="currentColor">
-            <path d="M87.823 60.7c-.463-.423-1.142-.506-1.695-.214-15.834 8.398-35.266 2.812-44.232-12.718-8.966-15.53-4.09-35.149 11.101-44.665.531-.332.796-.963.661-1.574-.134-.612-.638-1.074-1.259-1.153-9.843-1.265-19.59.692-28.193 5.66C13.8 12.041 6.356 21.743 3.246 33.35S1.732 57.08 7.741 67.487c6.008 10.407 15.709 17.851 27.316 20.961C38.933 89.486 42.866 90 46.774 90c7.795 0 15.489-2.044 22.42-6.046 8.601-4.966 15.171-12.43 18.997-21.586.61-1.453-.238-2.12-.698-2.543z"></path>
-          </svg>
-        ) : (
-          <div>
+        {theme === 'dark' ? (<div>
             <svg className='justify-center items-center ' xmlns="http://www.w3.org/2000/svg" viewBox="-15 -4 110 100" width="34" height="35" fill="currentColor">
               <path d="M45 68c-12.682 0-23-10.317-23-23 0-12.682 10.318-23 23-23 12.683 0 23 10.318 23 23 0 12.683-10.317 23-23 23zm0-40c-9.374 0-17 7.626-17 17s7.626 17 17 17 17-7.626 17-17-7.626-17-17-17zM45 17.556c-1.657 0-3-1.343-3-3V3c0-1.657 1.343-3 3-3s3 1.343 3 3v11.556c0 1.657-1.343 3-3 3zM45 90c-1.657 0-3-1.343-3-3V75.444c0-1.657 1.343-3 3-3s3 1.343 3 3V87c0 1.657-1.343 3-3 3zM14.556 48H3c-1.657 0-3-1.343-3-3s1.343-3 3-3h11.556c1.657 0 3 1.343 3 3s-1.343 3-3 3zM87 48H75.444c-1.657 0-3-1.343-3-3s1.343-3 3-3H87c1.657 0 3 1.343 3 3s-1.343 3-3 3zM66.527 26.473c-.768 0-1.535-.293-2.121-.878-1.172-1.172-1.172-3.071 0-4.243l8.171-8.171c1.172-1.172 3.07-1.171 4.242 0 1.172 1.172 1.172 3.071 0 4.243l-8.171 8.171c-.586.586-1.353.878-2.121.878zM15.302 77.698c-.768 0-1.536-.293-2.121-.879-1.172-1.171-1.172-3.071 0-4.242l8.171-8.171c1.171-1.172 3.071-1.172 4.242 0 1.172 1.171 1.172 3.071 0 4.242l-8.171 8.171c-.585.586-1.353.879-2.121.879zM23.473 26.473c-.768 0-1.536-.293-2.121-.878l-8.171-8.171c-1.172-1.172-1.172-3.071 0-4.243 1.172-1.172 3.072-1.171 4.243 0l8.171 8.171c1.172 1.172 1.172 3.071 0 4.243-.586.585-1.353.878-2.121.878zM74.698 77.698c-.768 0-1.535-.293-2.121-.879l-8.171-8.171c-1.172-1.171-1.172-3.071 0-4.242 1.172-1.172 3.07-1.172 4.242 0l8.171 8.171c1.172 1.171 1.172 3.071 0 4.242-.586.586-1.353.879-2.121.879z"/>
             </svg>
           </div>
+        ) : (
+          <div>
+          <svg className='justify-center items-center ' xmlns="http://www.w3.org/2000/svg" viewBox="-15 -4 110 100" width="34" height="35" fill="currentColor">
+            <path d="M87.823 60.7c-.463-.423-1.142-.506-1.695-.214-15.834 8.398-35.266 2.812-44.232-12.718-8.966-15.53-4.09-35.149 11.101-44.665.531-.332.796-.963.661-1.574-.134-.612-.638-1.074-1.259-1.153-9.843-1.265-19.59.692-28.193 5.66C13.8 12.041 6.356 21.743 3.246 33.35S1.732 57.08 7.741 67.487c6.008 10.407 15.709 17.851 27.316 20.961C38.933 89.486 42.866 90 46.774 90c7.795 0 15.489-2.044 22.42-6.046 8.601-4.966 15.171-12.43 18.997-21.586.61-1.453-.238-2.12-.698-2.543z"></path>
+          </svg></div>
         )}
       </button>
       </nav>
